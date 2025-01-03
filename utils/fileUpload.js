8aaed94cac52865b9ea2596f801bf7a06abab830
Abuzar <abuzarrrakhmonov@gmail.com>
2025-01-03 15:29:55 +0500

    .

--- utils/fileUpload.js
+++ utils/fileUpload.js
@@ -1,37 +1,43 @@
 const multer = require('multer');
 const path = require('path');
+const fs = require('fs');
 
-// Set storage
+// Upload papkasini yaratish
+const uploadPath = path.join(__dirname, '..', 'public', 'uploads');
+if (!fs.existsSync(uploadPath)) {
+    fs.mkdirSync(uploadPath, { recursive: true });
+}
+
+// Fayl saqlash konfiguratsiyasi
 const storage = multer.diskStorage({
     destination: function(req, file, cb) {
-        const uploadPath = path.join(__dirname, '..', 'public', 'uploads');
-        cb(null, uploadPath);
+        cb(null, uploadPath); // Faylni "public/uploads" papkasiga saqlash
     },
     filename: function(req, file, cb) {
-        cb(
-            null,
-            path.basename(file.originalname, path.extname(file.originalname)) + '-' + Date.now() + path.extname(file.originalname)
-        );
+        const fileName = `dish-${Date.now()}-${path.basename(file.originalname, path.extname(file.originalname))}${path.extname(file.originalname)}`;
+        cb(null, fileName); // Fayl nomini belgilash
     }
 });
 
-// upload
+// Fayl yuklash konfiguratsiyasi
 const upload = multer({
     storage,
-    limits: { fileSize: 10 * 1024 * 1024 }, // 10MB
+    limits: { fileSize: 10 * 1024 * 1024 }, // Maksimal fayl hajmi 10MB
     fileFilter(req, file, cb) {
-        checkFileTypes(file, cb);
+        checkFileTypes(file, cb); // Fayl turlarini tekshirish
     }
-});
+}).single('img'); // 'img' nomli faylni qabul qiladi
 
+// Fayl turlarini tekshirish
 function checkFileTypes(file, cb) {
     const filetypes = /jpeg|jpg|png|svg|webp|JFIF/;
     const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
     const mimetype = filetypes.test(file.mimetype);
+
     if (mimetype && extname) {
-        cb(null, true);
+        cb(null, true); // Fayl turi to'g'ri bo'lsa, uni qabul qilamiz
     } else {
-        cb(new Error('Only image (jpeg, jpg, png, svg, webp) are allowed'));
+        cb(new Error('Only image (jpeg, jpg, png, svg, webp) are allowed')); // Xato xabarini yuborish
     }
 }
 
